4b53233aa20f583d2ffc5fb929fe3322a5b5f9fe
aquibk580 <aquibk580@gmail.com>
2024-07-30 21:03:58 +0530

    fistcommit

---
+++ app/api/prompt/route.js
@@ -0,0 +1,14 @@
+import { connectToMongo } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (request) => {
+  try {
+    await connectToMongo();
+
+    const prompts = await Prompt.find({}).populate("creator");
+
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response("Failed To Fetch All Prompts", { status: 500 });
+  }
+};
---
+++ app/profile/page.jsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(`/api/users/${session?.user.id}/posts`);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setPosts(data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+
+    if (session?.user.id) {
+      fetchPosts();
+      console.log("Got id");
+    }
+  }, [session]);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+        const filteredPosts = posts.filter((p) => {
+          p._id !== post._id;
+        });
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile data"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
